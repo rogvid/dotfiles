377a11955a20e0c2f21104a9d71f1f12da1b236d
Rógvi Dávid Arge <rogvidarge@gmail.com>
2024-06-11 23:08:16 +0100

    Changed default keybindings for trouble to be <leader>t

--- nvim/.config/nvim/lua/plugins/development.lua
+++ nvim/.config/nvim/lua/plugins/development.lua
@@ -5,34 +5,34 @@ return {
     branch = 'dev', -- IMPORTANT!
     keys = {
       {
-        '<leader>xx',
+        '<leader>td',
         '<cmd>Trouble diagnostics toggle<cr>',
-        desc = 'Diagnostics (Trouble)',
+        desc = '[t]rouble [d]iagnostics',
       },
       {
-        '<leader>xX',
+        '<leader>tb',
         '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
-        desc = 'Buffer Diagnostics (Trouble)',
+        desc = '[t]rouble [b]uffer diagnostics',
       },
       {
-        '<leader>xs',
+        '<leader>ts',
         '<cmd>Trouble symbols toggle focus=false<cr>',
-        desc = 'Symbols (Trouble)',
+        desc = '[t]rouble [s]ymbols',
       },
       {
-        '<leader>cl',
+        '<leader>tl',
         '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
-        desc = 'LSP Definitions / references / ... (Trouble)',
+        desc = '[t]rouble [l]sp definitions / references / ... ',
       },
       {
-        '<leader>xL',
+        '<leader>tL',
         '<cmd>Trouble loclist toggle<cr>',
-        desc = 'Location List (Trouble)',
+        desc = '[t]rouble [L]ocation list',
       },
       {
-        '<leader>xQ',
+        '<leader>tq',
         '<cmd>Trouble qflist toggle<cr>',
-        desc = 'Quickfix List (Trouble)',
+        desc = '[t]rouble [q]uickfix list',
       },
     },
     dependencies = { 'nvim-tree/nvim-web-devicons' },

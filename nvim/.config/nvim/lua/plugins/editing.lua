2aa6047a14d1df0f10b08560a0a760bca86a40cf
Rógvi Dávid Arge <rogvidarge@gmail.com>
2024-06-12 00:11:27 +0100

    Added spectre

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  { -- Better search and replace
+    'nvim-pack/nvim-spectre',
+    build = false,
+    cmd = 'Spectre',
+    opts = { open_cmd = 'noswapfile vnew' },
+    -- stylua: ignore
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "[s]earch and [r]eplace (Spectre)" },
+    },
+  },
+}

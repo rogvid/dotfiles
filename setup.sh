ee97cf4716bd1a70134cdad82510fd0085e01a47
Rogvi David Arge <rogvidarge@gmail.com>
2025-02-10 08:50:08 +0000

    Changes to my dotfiles after trying out on fresh installation

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
+set -e
 
 if ! command -v stow &> /dev/null; then
     echo "stow could not be found, please install it first."
     exit 1
 fi
 
-if [ ! -d "$HOME/.dotfiles" ]; then
+if [ ! -d "$HOME/dotfiles" ]; then
 	echo "No dotfiles directory found. Cloning dotfiles repository..."
 	# Clone my dotfiles repository
-	git clone https://github.com/rogvid/.dotfiles.git $HOME/.dotfiles
+	git clone https://github.com/rogvid/dotfiles.git $HOME/dotfiles
 	#
 	# Since I make use of tmux I want to clone the tpm plugins folder
 	# to set up tmux
-	# if [ ! -d "$HOME/.tmux/plugins" ]; then mkdir $HOME/.tmux/plugins; fi
-	# if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm; fi
+	if [ ! -d "$HOME/.tmux/plugins" ]; then mkdir -p $HOME/.tmux/plugins; fi
+	if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm; fi
 
 	# Ensure that the config directory exists
 	if [ ! -d "$HOME/.config" ]; then mkdir $HOME/.config; fi
@@ -24,13 +25,6 @@ else
 	echo "Dotfiles directory found. Terminating initialization..."
 fi
 
-if ! command -v nix-env &> /dev/null; then
-    echo "Please make sure nix is installed"
-    exit 1
-fi
-
 # all: stow --verbose --target=$$HOME --restow */
 # delete: stow --verbose --target=$$HOME --delete */
-# Install nix packages
-nix-env -iA nixpkgs.myPackages
 
